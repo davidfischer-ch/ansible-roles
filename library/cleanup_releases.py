1bc2e71c783b40a7774680a613385a0521578567
David Fischer <david.fischer.ch@gmail.com>
2017-03-06 16:48:31 +0100

    Add ok_flag argument + remove default for regexp argument

--- library/cleanup_releases.py
+++ library/cleanup_releases.py
@@ -24,17 +24,23 @@ options:
     description:
       - Number of releases to keep.
       - Default value means keep all.
-  regexp:
+  ok_flag:
     required: false
-    default: '[0-9]{8}T[0-9]{12}'
+    default: null
     description:
-      - Default corresponds to a short ISO 8601 date-time.
+      - A file inside the release directory that flags the release as complete.
+      - WARNING: All releases without the flag will ALWAYS be removed!
+      - Default value means this feature is disabled.
+  regexp:
+    required: true
+    description:
       - The regular expression to look for in the name of the releases directories.
         Uses Python regular expressions; see U(http://docs.python.org/2/library/re.html).
 """
 
 EXAMPLES = r"""
 - cleanup_releases: directory=/var/app/releases keep=3
+- cleanup_releases: directory=/var/app/releases keep=3 ok_flag=ok.flag
 - cleanup_releases: directory=/var/app/releases keep=3 regexp='release-[0-9]{3}'
 """
 
@@ -58,24 +64,30 @@ def main():
         argument_spec=dict(
             directory=dict(required=True, type='path'),
             keep=dict(default=None, type='int'),
-            regexp=dict(default=r'[0-9]{8}T[0-9]{12}')
+            ok_flag=dict(default=None),
+            regexp=dict(required=True)
         ),
         supports_check_mode=True
     )
-    directory, keep, regexp = (module.params[k] for k in ('directory', 'keep', 'regexp'))
+    directory, keep, ok_flag, regexp = (module.params[k] for k in ('directory', 'keep', 'ok_flag', 'regexp'))
     regexp = re.compile(regexp)
     if keep is not None and keep < 1:
         module.fail_json(msg="'keep' should be a positive number")
 
     # FIXME first should be removed releases that don't have the "complete" flag
-    releases = (os.path.join(directory, r) for r in os.listdir(directory) if regexp.match(r))
-    oldest_releases = [] if keep is None else sorted(releases, reverse=True)[keep:]
-    before = [{'path': r, 'state': get_state(r)} for r in oldest_releases]
+    releases, remove_releases = set(os.path.join(directory, r) for r in os.listdir(directory) if regexp.match(r)), set()
+    if ok_flag is not None:
+        remove_releases = set(r for r in releases if not os.path.exists(os.path.join(r, ok_flag)))
+        releases = releases - remove_releases
+    if keep is not None:
+        remove_releases.update(sorted(releases, reverse=True)[keep:])
+    remove_releases = sorted(remove_releases, reverse=True)  # For cosmetic reasons
+    before = [{'path': r, 'state': get_state(r)} for r in remove_releases]
     if not module.check_mode:
-        for release in oldest_releases:
+        for release in remove_releases:
             shutil.rmtree(release)
-    module.exit_json(changed=bool(oldest_releases), diff={
-        'after': [{'path': r, 'state': 'absent'} for r in oldest_releases], 'before': before
+    module.exit_json(changed=bool(remove_releases), diff={
+        'after': [{'path': r, 'state': 'absent'} for r in remove_releases], 'before': before
     })
 
 if __name__ == '__main__':
