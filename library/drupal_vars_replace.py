2190e8961dcb713756c075bbc67cc8bd7a1fa3cb
David Fischer <david.fischer.ch@gmail.com>
2017-03-15 11:33:50 +0100

    Favor drush from vendors

--- library/drupal_vars_replace.py
+++ library/drupal_vars_replace.py
@@ -19,6 +19,9 @@ options:
     required: true
   value:
     required: true
+  drush:
+    required: false
+    default: vendor/bin/drush
 """
 
 EXAMPLES = r"""
@@ -26,9 +29,9 @@ EXAMPLES = r"""
 """
 
 
-def get_drupal_variables(directory):
+def get_drupal_variables(drush, directory):
     return json.loads(subprocess.check_output(
-        ['drush', '-r', directory, 'vget', '--format=json'], env=get_drush_environment()
+        [drush, '-r', directory, 'vget', '--format=json'], env=get_drush_environment()
     ).decode('utf-8'))
 
 
@@ -40,7 +43,7 @@ def get_drush_environment():
 get_drush_environment.env = None
 
 
-def replace_drupal_variables(directory, pattern, string, simulate=False):
+def replace_drupal_variables(drush, directory, pattern, string, simulate=False):
     updated_variables = []
     regexp = re.compile(pattern)
 
@@ -51,20 +54,21 @@ def replace_drupal_variables(directory, pattern, string, simulate=False):
             return [update(v) for v in value]
         return regexp.sub(string, value) if isinstance(value, unicode) else value
 
-    for name, value in get_drupal_variables(directory).items():
+    for name, value in get_drupal_variables(drush, directory).items():
         updated_value = update(value)
         if updated_value != value:
             if not simulate:
-                set_drupal_variable(directory, name, updated_value)
+                set_drupal_variable(drush, directory, name, updated_value)
             updated_variables.append({
                 'after': updated_value, 'after_header': name, 'before': value, 'before_header': name
             })
     return updated_variables
 
 
-def set_drupal_variable(directory, name, value):
-    process = subprocess.Popen(['drush', '-r', directory, 'vset', '--format=json', name, json.dumps(value)],
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=get_drush_environment())
+def set_drupal_variable(drush, directory, name, value):
+    process = subprocess.Popen([drush, '-r', directory, 'vset', '--format=json', name, json.dumps(value)],
+                               cwd=directory, env=get_drush_environment(),
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (stdout, stderr), returncode = process.communicate(), process.poll()
     if returncode:
         raise ValueError(name, value, stdout, stderr)
@@ -73,14 +77,15 @@ def set_drupal_variable(directory, name, value):
 def main():
     module = AnsibleModule(
         argument_spec=dict(
+            drush=dict(required=False, type='path'),
             path=dict(required=True, type='path'),
             pattern=dict(required=True),
             value=dict(required=True)
         ),
         supports_check_mode=True
     )
-    path, pattern, value = (module.params[k] for k in ('path', 'pattern', 'value'))
-    updated_variables = replace_drupal_variables(path, pattern, value, simulate=module.check_mode)
+    drush, path, pattern, value = (module.params[k] for k in ('drush', 'path', 'pattern', 'value'))
+    updated_variables = replace_drupal_variables(drush, path, pattern, value, simulate=module.check_mode)
     module.exit_json(changed=bool(updated_variables), diff=updated_variables)
 
 if __name__ == '__main__':
