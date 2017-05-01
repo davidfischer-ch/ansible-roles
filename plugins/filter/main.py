82679c0b34a82977ffc7b1b7939fc4c3f1c953e6
David Fischer <david.fischer.ch@gmail.com>
2017-05-01 11:01:55 +0200

    Add filter format_items

--- plugins/filter/main.py
+++ plugins/filter/main.py
@@ -1,6 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 
+def format_items(items, pattern):
+    return [pattern.format(i) for i in items]
+
+
 def oldest_ec2_snapshots(snapshots, keep):
     return sorted(snapshots, key=lambda s: s['start_time'])[keep:]
 
@@ -10,5 +14,6 @@ class FilterModule(object):
 
     def filters(self):
         return {
+            'format_items': format_items,
             'oldest_ec2_snapshots': oldest_ec2_snapshots
         }
