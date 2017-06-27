a2a1dc86e8543096c68a238e0f17287a04ebff75
David Fischer <david.fischer.ch@gmail.com>
2017-06-27 10:14:52 +0200

    Add chunk and enumerate filters

--- plugins/filter/main.py
+++ plugins/filter/main.py
@@ -1,5 +1,31 @@
 # -*- encoding: utf-8 -*-
 
+import itertools
+
+
+def chunk(objects, length, of_type=list):
+    """
+    Yield successive chunks of defined `length` from `objects`. Last chunk may be smaller.
+    Copied from https://github.com/davidfischer-ch/pytoolbox/blob/master/pytoolbox/itertools.py#L34.
+
+    **Example usage**
+
+    >>> iterable = iter(range(7))
+    >>> assert list(chunk(iterable, 3)) == [[0, 1, 2], [3, 4, 5], [6]]
+    >>> assert list(chunk(iterable, 3)) == []
+    >>> assert list(chunk((0, 1, (2, 3)), 1, of_type=set)) == [{0}, {1}, {(2, 3)}]
+    """
+    iterable = iter(objects)
+    while True:
+        chunk = of_type(itertools.islice(iterable, 0, length))
+        if not chunk:
+            break
+        yield chunk
+
+
+def enumerate_(objects, *args, **kwargs):
+    return ({'index': i, 'items': o} for i, o in enumerate(objects, *args, **kwargs))
+
 
 def format_items(items, pattern):
     return [pattern.format(i) for i in items]
@@ -14,6 +40,12 @@ class FilterModule(object):
 
     def filters(self):
         return {
+            'chunk': chunk,
+            'enumerate': enumerate_,
             'format_items': format_items,
             'oldest_ec2_snapshots': oldest_ec2_snapshots
         }
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
