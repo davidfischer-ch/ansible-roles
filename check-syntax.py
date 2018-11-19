8cb0b779c803b65e1de4d267da50d9a08ec1fe4c
David Fischer <david.fischer.ch@gmail.com>
2018-11-19 09:01:56 +0100

    Add optional directory argument

--- check-syntax.py
+++ check-syntax.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
-import yaml
+import argparse, yaml
 
 from pytoolbox import filesystem
+from pytoolbox.argparse import is_dir, FullPaths
 
-for filename in sorted(filesystem.find_recursive('.', '*.yml')):
-    print(filename)
-    with open(filename) as f:
-        yaml.load(f)
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     epilog='Check YAML syntax of a whole file tree.')
+    parser.add_argument('directory', action=FullPaths, nargs='?', type=is_dir, default='.')
+    args = parser.parse_args()
+    check(args.directory)
+
+
+def check(directory):
+    for filename in sorted(filesystem.find_recursive(directory, '*.yml')):
+        print(filename)
+        with open(filename) as f:
+            yaml.load(f)
+
+
+if __name__ == '__main__':
+    main()
