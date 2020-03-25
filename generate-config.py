1fe11518827138843bb3a78119966db267bac2a9
David Fischer <david@fisch3r.net>
2020-03-25 11:40:26 +0100

    Update list of disabled and blacklisted options

--- generate-config.py
+++ generate-config.py
@@ -4,17 +4,25 @@ import argparse, collections, datetime, subprocess, yaml
 
 import jinja2
 
-OPTIONS_BLACKLIST = {'CONNECTION_FACTS_MODULES'}
+OPTIONS_BLACKLIST = {
+    'CONNECTION_FACTS_MODULES',
+    'DEFAULT_HANDLER_INCLUDES_STATIC',  # Deprecated even if not marked as deprecated...
+    'DEFAULT_SQUASH_ACTIONS',           # Deprecated even if not marked as deprecated...
+    'DEFAULT_TASK_INCLUDES_STATIC',     # Deprecated even if not marked as deprecated...
+}
+OPTIONS_DISABLED = {
+    'DEFAULT_VAULT_IDENTITY_LIST',  # Default value is broken, at least using this tool
+}
 
 TEMPLATE = """# Generator: ./generate-config.py
 # Date: {{ date }}
 {% for section, options in config.items()|sort %}
 [{{ section }}]
-{%- for key, default, name, description in options|sort %}
+{%- for key, default, name, description, enabled in options|sort %}
 {% for line in description %}
 # {{ line }}
 {%- endfor %}
-{% if default != None %}{{ key }} = {{ default }}{% else %}# {{ key }} ={% endif -%}
+{% if enabled %}{{ key }} = {{ default }}{% else %}# {{ key }} ={% endif -%}
 {%- endfor %}
 {% endfor -%}"""
 
@@ -26,12 +34,12 @@ def main():
     args = parser.parse_args()
 
     config = collections.defaultdict(set)
-    for section, key, meta in get_options():
+    for section, key, meta, enabled in get_options():
         default, description, name = (
             globals()[f'clean_{k}'](meta.get(k))
             for k in ('default', 'description', 'name')
         )
-        config[section].add((key, default, name, description))
+        config[section].add((key, default, name, description, enabled and default is not None))
 
     templates = jinja2.Environment(loader=jinja2.BaseLoader).from_string(TEMPLATE)
     content = templates.render(config=config, date=datetime.datetime.utcnow())
@@ -63,7 +71,7 @@ def get_options():
             continue
         key, section = (ini[k] for k in ('key', 'section'))
         if env_name not in OPTIONS_BLACKLIST:
-            yield section, key, meta
+            yield section, key, meta, env_name not in OPTIONS_DISABLED
 
 
 if __name__ == '__main__':
