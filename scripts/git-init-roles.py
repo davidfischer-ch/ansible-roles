bc4efc5c55161717315e210904a4a1a255a4d5dc
David Fischer <david.fischer.ch@gmail.com>
2021-09-24 09:54:00 +0200

    Clone roles using ssh protocol

--- scripts/git-init-roles.py
+++ scripts/git-init-roles.py
@@ -8,21 +8,24 @@ roles = Path('roles').resolve()
 with open(roles / 'requirements.yml') as f:
     requirements = yaml.safe_load(f.read())
 
-
 for requirement in requirements:
     name = requirement['name']
-    src = requirement['src']
+    url = requirement['src']
     version = requirement['version']
 
-    directory = roles / name
-    git_directory = directory / 'git'
-    run = functools.partial(subprocess.run, cwd=directory, capture_output=True)
+    url = url.replace('https://', 'git@').replace('github.com/', 'github.com:') + '.git'
+    target = roles / name
+    target_git = target / '.git'
 
-    if git_directory.exists():
-        print(f'[{name}] Remove {git_directory}')
-        shutil.rmtree(git_directory)
+    run = functools.partial(subprocess.run, cwd=target, capture_output=True)
 
-    with tempfile.TemporaryDirectory() as tmp:
-        print(f'[{name}] Cloning version {version} from {src}')
-        run(['git', 'clone', src, tmp, '--branch', version])
-        shutil.move(Path(tmp) / '.git', git_directory)
+    if target_git.exists():
+        print(f'[{name}] Remove {target_git}')
+        shutil.rmtree(target_git)
+
+    with tempfile.TemporaryDirectory() as source:
+        source_git = Path(source) / '.git'
+        print(f'[{name}] Cloning version {version} from {url}')
+        run(['git', 'clone', url, source, '--branch', version])
+        # bugs.python.org/issue32689
+        shutil.move(str(source_git), target_git)
